88b8644a966e910b50b5737774e6456d25fee0c0
jfaure <julie.faure-lacroix@calculquebec.ca>
2021-11-03 12:10:20 -0400

    Update pv_soumission.sh

--- codes/pv_soumission.sh
+++ codes/pv_soumission.sh
@@ -5,6 +5,6 @@
 #SBATCH --mem-per-cpu=2048M      # memory; default unit is megabytes
 #SBATCH --time=0-00:15           # time (DD-HH:MM)
 
-module load gcc/9.3.0 paraview-offscreen/9.3.1
+module load gcc/9.3.0 paraview-offscreen
 cd ./paraview_national/codes
 pvbatch --force-offscreen-rendering writeImage.py
