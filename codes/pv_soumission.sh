874bd92aa12d8e2b0faacde4396156ad407fee92
jfaure <julie.faure-lacroix@calculquebec.ca>
2021-11-03 11:55:59 -0400

    Create pv_soumission.sh

---
+++ codes/pv_soumission.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+#SBATCH --nodes=1                # number of nodes
+#SBATCH --cpus-per-task=1        # number of processes
+#SBATCH --mem-per-cpu=2048M      # memory; default unit is megabytes
+#SBATCH --time=0-00:15           # time (DD-HH:MM)
+
+module load gcc/9.3.0 paraview/9.3.1
+cd ./paraview_national/codes
+pvbatch --force-offscreen-rendering writeImage.py
